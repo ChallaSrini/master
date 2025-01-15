d45dc2487d9a12ef99e7fa823bc26d11cfe5c1b6
SRINIVASA CHALLA REDDY <challa.srinivascse99@gmail.com>
2025-01-15 11:39:48 +0000

    latest changes

---
+++ models/views/dim_mart_world_population_vw.sql
@@ -0,0 +1,16 @@
+SELECT s2.country_cd,
+       s1.country as country_name,
+       population,
+       yearly_change,
+       net_change,
+       density,
+       land_area,
+       migrants,
+       fert_rate,
+       med_age,
+       urban_pop,
+       world_share
+ FROM {{ ref('dim_mart_world_population') }} s1
+ join {{ source("my_src","cntry_lkp")}}
+   on s1.country=s2.country_name
+where s1.effective_to='9999-01-01'
