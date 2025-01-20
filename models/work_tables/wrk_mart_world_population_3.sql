90c49c3c36593425068ded08d7b7f93d7345da35
SRINIVASA CHALLA REDDY <challa.srinivascse99@gmail.com>
2025-01-20 08:06:20 +0000

    testing of relation exists macro

--- models/work_tables/wrk_mart_world_population_3.sql
+++ models/work_tables/wrk_mart_world_population_3.sql
@@ -1,22 +1,17 @@
+ {{ config(materialized="table") }}
 
-{{ config(materialized='table') }}
-
-{%- set source_relation = adapter.get_relation(
-      database=this.database,
-      schema=this.schema,
-      identifier=source("my_src","tgt_population_test").table) -%}
-
-{% if source_relation.table == source("my_src","tgt_population_test").table %}
-{{ log("Source Relation: " ~ source_relation , info=true) }}
+{% if relation_exists(source("my_src", "tgt_population_test")) %}
+    {{ log("Source Relation: " ~ source("my_src", "tgt_population_test"), info=true) }}
     select *
-      from {{ ref('wrk_mart_world_population') }} s1
+    from {{ ref("wrk_mart_world_population") }} s1
 {% else %}
-{{ log("Source Relation doesn't exist: " ~ source("my_src","tgt_population_test").table, info=true) }}
-    select *,'extra_column' as ext_col
-      from {{ ref('wrk_mart_world_population') }} s1
+    {{
+        log(
+            "Source Relation doesn't exist: "
+            ~ source("my_src", "tgt_population_test").table,
+            info=true,
+        )
+    }}
+    select *, 'extra_column' as ext_col
+    from {{ ref("wrk_mart_world_population") }} s1
 {% endif %}
-
-
-
-
- 
