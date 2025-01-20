4d4bb21e7ba84b431e1c8bb2cf1d50acc04d3b7a
SRINIVASA CHALLA REDDY <challa.srinivascse99@gmail.com>
2025-01-20 10:24:02 +0000

    test

--- models/Marts/Dim/SCD-1/dim_mart_world_population_typ1.sql
+++ models/Marts/Dim/SCD-1/dim_mart_world_population_typ1.sql
@@ -1,6 +1,6 @@
 {{ config(
     unique_key='dim_cntry_key', 
-    merge_update_columns=['country','population','yearly_change','net_change','density','land_area','migrants','fert_rate','med_age','urban_pop','world_share','updt_ts','load_datetime','compare_key']
+    merge_update_columns=['country','population','yearly_change','net_change','density','land_area','migrants','fert_rate','med_age','urban_pop','world_share','updt_ts','last_updt_dt','compare_key']
 ) }}
 
 SELECT dim_cntry_key,
