b2c97351acd2de9f8d9bacfe40ef76d13839c428
SRINIVASA CHALLA REDDY <challa.srinivascse99@gmail.com>
2025-01-20 14:22:25 +0000

    one more for profile

--- models/Marts/Dim/Profile/dim_mart_churn_profile.sql
+++ models/Marts/Dim/Profile/dim_mart_churn_profile.sql
@@ -1,13 +1,13 @@
 {% if relation_exists( this ) %}
  {{ log("Target table existis so running this block- ", info=true) }}
-   select dim_churn_prfl_key,
-          tenure,
-          numofproducts,
-          hascrcard,
-          isactivemember,
-          exited,
-          eff_cal_dim_id,
-          inst_ts
+   select s1.dim_churn_prfl_key,
+          s1.tenure,
+          s1.numofproducts,
+          s1.hascrcard,
+          s1.isactivemember,
+          s1.exited,
+          s1.eff_cal_dim_id,
+          s1.inst_ts
      from {{ ref('wrk_mart_churn_profile') }} s1
 left join {{this}} s2
        on s1.dim_churn_prfl_key=s2.dim_churn_prfl_key
