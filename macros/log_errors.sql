232a0a88640bfb4157556e72e888a8fdf96dd34d
SRINIVASA CHALLA REDDY <challa.srinivascse99@gmail.com>
2025-01-22 12:22:09 +0000

    one more change

--- macros/log_errors.sql
+++ macros/log_errors.sql
@@ -1,39 +1,27 @@
-{% macro log_errors(results) %}
-    {{ log("========== Begin log errors ==========", info=True) }}
+{% macro log_errors() %}
     {% if execute %}
-        -- Get the invocation_id
-        {% set invocation_id = invocation_id %}
-
-        -- Create a list to store rows for the CTE
-        {% set rows = [] %}
-		{% set ins_rows = [] %}
-
-        {% for res in results -%}
+        {{ log("Execution mode after DBT run: TRUE", info=True) }}
+		{% set error_rows = [] %}
+		{% for res in results -%}
 			{%- if res.status == 'error' %}
-				{% set line = "node: " ~ res.node.unique_id ~ "; status: " ~ res.status %}
-				{{ log(line, info=True) }}
-
-				{% do ins_rows.append("('" ~ invocation_id ~ "', '" ~ res.node.unique_id.split('.')[-1] ~ "', '" ~ res.status ~ "', '"~ res.message[:500] ~"' )") %}
-				{% do rows.append("('" ~ invocation_id ~ "', '" ~ res.node.unique_id.split('.')[-1] ~ "', '" ~ res.status ~ "', '"~ res.message[:500] ~"' )") %}
-			{% else %}
-                {% do rows.append("('" ~ invocation_id ~ "', '" ~ res.node.unique_id.split('.')[-1] ~ "', '" ~ res.status ~ "', '"~ res.message[:500] ~"' )") %}
+				{% do error_rows.append("('" ~ invocation_id ~ "', '" ~ res.node.unique_id.split('.')[-1] ~ "', '" ~ res.status ~ "', '"~ res.message[:500] ~"' )") %}
 			{%- endif %}
-				
-        {%- endfor %}
-        {{ log("Insert rows" ~ ins_rows)}}
-        {% if ins_rows %}
-        -- Build the CTE
-            {% set ins_cte_query = "WITH results_cte AS (\n  SELECT * FROM (VALUES\n    " ~ ins_rows | join(',\n    ') ~ "\n  ) AS t(invocation_id, unique_id, status,message)\n) " %}
-		
-		    {% set error_query= "INSERT INTO " ~ env_var('DBT_AUDIT_SCHEMA') ~ ".dbt_error_dtl \n" ~ins_cte_query~ "SELECT invocation_id, unique_id, message, current_timestamp() FROM results_cte " %}
-		
-		    {{ log("Generated Error Insertion Query:\n" ~ error_query, info=True) }}
-            {{ return(error_query) }}
-        {% else %}
-            {{ return("-- No execution occurred (dry run).") }}
+		{%- endfor %}
+		{% if error_rows %}
+			{% set error_cte_query = "WITH results_cte AS (\n  SELECT * FROM (VALUES\n    " ~ error_rows | join(',\n    ') ~ "\n  ) AS t(invocation_id, unique_id, status,message)\n) " %}
+			{% set error_query= "INSERT INTO " ~ env_var('DBT_AUDIT_SCHEMA') ~ ".dbt_error_dtl \n" ~error_cte_query~ "SELECT invocation_id, unique_id, message, current_timestamp() FROM results_cte; " %}
+		{% else %}
+			{% set error_query= " select 'dummy'" %}
+		{% endif %}
     {% else %}
-        {{ return("-- No execution occurred (dry run).") }}
+        {{ log("Execution mode after DBT run: FALSE", info=True) }}
+		{% set error_query= "select 'dummy'" %}
     {% endif %}
-
-    {{ log("========== End log errors ==========", info=True) }}
+{#    {{ log("Results metadata: " ~ results, info=True) }} #}
+    {{ log("Results audit- Completed") }}
+    {{ return(error_query) }}
 {% endmacro %}
+
+
+
+
--- macros/log_status.sql
+++ macros/log_status.sql
@@ -1,28 +1,12 @@
-{% macro log_status(results) %}
-    {{ log("========== Begin log status test==========" ~ execute, info=True) }}
+{% macro log_status() %}
     {% if execute %}
-        -- Get the invocation_id
-        {% set invocation_id = invocation_id %}
-
-        -- Create a list to store rows for the CTE
-        {% set rows = [] %}
-		{% set ins_rows = [] %}
-
-        {% for res in results -%}
-			{%- if res.status == 'error' %}
-				{% set line = "node: " ~ res.node.unique_id ~ "; status: " ~ res.status %}
-				{{ log(line, info=True) }}
-
-				{% do ins_rows.append("('" ~ invocation_id ~ "', '" ~ res.node.unique_id.split('.')[-1] ~ "', '" ~ res.status ~ "', '"~ res.message[:500] ~"' )") %}
-				{% do rows.append("('" ~ invocation_id ~ "', '" ~ res.node.unique_id.split('.')[-1] ~ "', '" ~ res.status ~ "', '"~ res.message[:500] ~"' )") %}
-			{% else %}
-                {% do rows.append("('" ~ invocation_id ~ "', '" ~ res.node.unique_id.split('.')[-1] ~ "', '" ~ res.status ~ "', '"~ res.message[:500] ~"' )") %}
-			{%- endif %}
-				
-        {%- endfor %}
-
-
-        {% set upd_query = "WITH results_cte AS (\n  SELECT * FROM (VALUES\n    " ~ rows | join(',\n    ') ~ "\n  ) AS t(invocation_id, unique_id, status,message)\n)
+        {{ log("Execution mode after DBT build: TRUE", info=True) }}
+		{% set status_rows = [] %}
+		{% for res in results -%}
+            {% do status_rows.append("('" ~ invocation_id ~ "', '" ~ res.node.unique_id.split('.')[-1] ~ "', '" ~ res.status ~ "', '"~ res.message[:500] ~"' )") %}
+		{%- endfor %}
+		{% if status_rows %}
+			        {% set status_query = "WITH results_cte AS (\n  SELECT * FROM (VALUES\n    " ~ status_rows | join(',\n    ') ~ "\n  ) AS t(invocation_id, unique_id, status,message)\n)
             merge into "  ~ env_var('DBT_AUDIT_SCHEMA') ~".dbt_run_dtl_info tgt
                  using results_cte src
                     on tgt.invocation_id = src.invocation_id 
@@ -30,11 +14,18 @@
                   when matched and tgt.end_time is null
                   then update 
                    set tgt.status =src.status,tgt.end_time = current_timestamp()   " %}
-		
-		{{ log("Generated Merge Query:\n" ~ upd_query, info=True) }}
-        {{ return(upd_query) }}
+		{% else %}
+			{% set status_query= "select 'dummy'" %}
+		{% endif %}		
     {% else %}
-        {{ return("-- No execution occurred (dry run).") }}
+        {{ log("Execution mode after DBT build: FALSE", info=True) }}
+		{% set status_query= "select 'dummy'" %}
     {% endif %}
-    {{ log("========== End log status ==========", info=True) }}
+  {#  {{ log("Results metadata: " ~ results, info=True) }} #}
+    {{ log("Results audit- Completed") }}
+    {{ return(status_query) }}
 {% endmacro %}
+
+
+
+
