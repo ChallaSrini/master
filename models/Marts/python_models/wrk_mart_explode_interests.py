ad311622264888f8ebd1fb07f982af6d0d5c600b
SRINIVASAREDDY CHALLA <srinivas.challa@apexon.com>
2025-02-17 12:55:56 +0000

    python model changes

--- models/Marts/python_models/wrk_mart_explode_interests.py
+++ models/Marts/python_models/wrk_mart_explode_interests.py
@@ -2,12 +2,7 @@ import pandas as pd
 from pyspark.sql.functions import split, explode
 
 def model(dbt, session):
-
-    dbt.config(
-          materialized="table"
-    )  
-    
-
+   
     df = dbt.source("my_src","src_lkp_exports")
 
     df_transformed = df.withColumn("interest", explode(split(df["interest"], ", "))).toPandas()
